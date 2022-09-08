cd5e0c7cb97872e1b8d0ded8a7d350e9ad1a663a
Menomcking <patrik8870@gmail.com>
2022-09-08 12:48:18 +0200

    kész minden

---
+++ feladat2.js
@@ -0,0 +1,5 @@
+let array = [];
+let o {
+    name = "",
+    age = "",
+}
---
+++ feladat3.js
@@ -0,0 +1,10 @@
+/*setInterval(hurray, 2000);
+function hurray(){
+    console.log("Hurray");
+}*/
+setInterval(count, 2000);
+let i = 0;
+    function count(){
+        console.log(i);
+        i++;
+    }
