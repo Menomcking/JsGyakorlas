1ce93f163ae42c0d3811825fef0a55c04be4a051
Menomcking <patrik8870@gmail.com>
2022-09-08 13:10:48 +0200

    2.feladat_befejezve

--- feladat1.js
+++ feladat1.js
@@ -1,7 +1,4 @@
 let array = [];
 for(let i = 0; i < 10; i++){
-    array[i] = Math.round((Math.random() * 100) + 1);
-}
-for(let i = 0; i < 10; i++){
-    console.log(array[i]);
+    array[i] = Math.round((Math.random() * 99) + 1);
 }
--- feladat2.js
+++ feladat2.js
@@ -1,5 +1,13 @@
 let array = [];
-let o {
-    name = "",
-    age = "",
+
+for(let i = 0; i < 20; i++){
+    array.push({
+        nev: "Cirmos"+ (i + 1),
+        eletkor: Math.floor(Math.random() * 9) + 2,
+    });
+}
+for(let i = 0; i < 20; i++){
+    if(array[i].eletkor == 2){
+        console.log(array[i].nev);
+    }
 }
